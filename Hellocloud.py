4239752dda8427dd4b21dcd4fc1622d21d6913e8
sirinda <you@example.com>
2021-01-18 21:41:40 +0700

    Change new msg

--- Hellocloud.py
+++ Hellocloud.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "i love suchonnnnn"
+    return "Hello SIRINDA POONPUNNN"
 
 if __name__ =="__main__":
     server.run(host='0.0.0.0', port=80)
